950be8696367966eff9a89fce901799f24d41214
DESKTOP-LIRSJS6\Klova <nikolayklova404@gmail.com>
2024-05-21 11:43:54 +0300

    adding dao

--- ExchangeOffice.Core/DAO/Contact.cs
+++ ExchangeOffice.Core/DAO/Contact.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ExchangeOffice.Core.DAO {
 	public class Contact {
+		public Guid Id { get; set; }
 		public string? FullName { get; set; }
 		public string? Email { get; set; }
 		public string? PhoneNumber { get; set; }
